da7b678b753586f5d3e6bda39d52595f21b626c9
Gio <44237862+Redifexx@users.noreply.github.com>
2025-09-26 15:39:29 -0400

    module setup

---
+++ dist/app-bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+var text = "Hello World";
+console.log(text);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLWJ1bmRsZS5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsSUFBTSxJQUFJLEdBQVcsYUFBYSxDQUFDO0FBQ25DLE9BQU8sQ0FBQyxHQUFHLENBQUMsSUFBSSxDQUFDLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZWRpZmV4eC5naXRodWIuaW8vLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgdGV4dDogc3RyaW5nID0gXCJIZWxsbyBXb3JsZFwiO1xyXG5jb25zb2xlLmxvZyh0ZXh0KTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
